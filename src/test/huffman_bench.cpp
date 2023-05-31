097a3158e821481ee0adcdb51b9fc184a81e89d8
Gary Miguel <garymm@garymm.org>
2023-05-30 21:28:02 -0700

    support build version, record benchmark results
    Change-Id: I7a8331e1e27f61dfa1c485f1c0c8cc3b9435be0f

--- src/test/huffman_bench.cpp
+++ src/test/huffman_bench.cpp
@@ -1,12 +1,14 @@
 #include <benchmark/benchmark.h>
 
 #include "src/huffman.hpp"
+#include "src/version/version.hpp"
 
 static void BM_CodeTable(benchmark::State& state) {
   const auto frequencies = std::vector<std::pair<char, std::size_t>>{
         {'e', 100}, {'n', 20}, {'x', 1}, {'i', 40}, {'q', 3}};
   constexpr auto eot = char{4};
 
+  state.SetLabel(gpu_deflate::Version::full_version_string);
   for (auto _ : state) {
     gpu_deflate::code_table ct{frequencies, eot};
     benchmark::DoNotOptimize(ct);
