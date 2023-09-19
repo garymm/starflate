50bac46efda7b58fd816681b33fdb96f2544a9a3
Gary Miguel <garymm@garymm.org>
2023-09-19 16:19:42 +0000

    WIP gunzip
    Change-Id: I65fd59b5163ca3cb128c830e9bd75f7ba7440940

---
+++ src/gunzip.cpp
@@ -0,0 +1,9 @@
+#include "gunzip.hpp"
+
+namespace starflate {
+auto gunzip(std::istream& in, std::ostream& out) -> GunzipError
+{
+  // TODO: implement
+  return GunzipError::NoError;
+}
+}  // namespace starflate
